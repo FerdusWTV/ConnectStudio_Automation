5ca822c7446a4e8dda2b0467b6982531aa74aff4
ferdus615 <ferdus615@gmail.com>
2025-09-10 22:35:37 +0600

    branding automation done

--- Automated_Portal_Creation.py
+++ Automated_Portal_Creation.py
@@ -40,34 +40,15 @@ wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'swal2-container')))
 # ======================================================================================================================
 
 #dashboard page validation
-welcome = driver.find_element(By.CLASS_NAME, 'header-title').text
+welcome = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'header-title'))).text
 print(f'Login successful: {welcome}')
 assert 'Welcome' in welcome
 
 # ======================================================================================================================
 
-#org page access validation
-driver.find_element(By.XPATH, "//li[3]").click()
-org_page = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "page-header-title")))
-org_page_text = org_page.text
-print(f'Accessed Organization Page successfully: {org_page_text}')
-assert 'All Organizations' in org_page_text
-
-# ======================================================================================================================
-
-# navigate to the targeted organization
-# search for the org
-driver.find_element(By.CLASS_NAME, "connect-studio-search-input-small").send_keys("Automated")
-
-# click the org
-try:
-    wait.until(EC.presence_of_element_located((By.XPATH, "//h6[normalize-space()='Automated Test ORG']")))
-    driver.find_element(By.CLASS_NAME, "org-card-arrow-icon").click()
-except:
-    print('Org not found')
-    
-org_name = driver.find_element(By.CLASS_NAME, "client-org-name").text
-print(f'Organization Name: {org_name}')
+create_portal_btn = driver.find_element(By.XPATH, "//div[contains(text(),'Create Portal')]")
+portal_btn_text = create_portal_btn.text
+print(f"portal_btn_text: {portal_btn_text}")
 
 # ======================================================================================================================
 
@@ -78,7 +59,7 @@ driver.find_element(By.XPATH, "//li[@class='show']").click()
 # branding page fillup
 
 # select organization
-driver.find_element(By.ID, "rc_select_0").click()
+wait.until(EC.presence_of_element_located((By.ID, "rc_select_0"))).click()
 org_menu = driver.find_element(By.XPATH, "//div[@class='rc-virtual-list-holder']")
 driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", org_menu)
 driver.find_element(By.XPATH, "//div[@title='Automated Test ORG']").click()
@@ -87,28 +68,58 @@ driver.find_element(By.XPATH, "//div[@title='Automated Test ORG']").click()
 driver.find_element(By.ID, "rc_select_1").click()
 client_menu = driver.find_element(By.CLASS_NAME, "rc-virtual-list-holder-inner")
 driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", client_menu)
-driver.find_element(By.XPATH, "//div[contains(text(),'AUTO')]").click()
+driver.find_element(By.XPATH, "//div[contains(text(),'Automated client 002')]").click()
 
 # Template style (optional)
 driver.find_element(By.XPATH, "//div[@data-testid='template-style']").click()
+time.sleep(1)
 template = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@title='Material - popular user experience style comprised of 3D elements']")))
 template.click()
 
+
 # header menu logo
-# logo = driver.find_element(By.XPATH, "(//input[@type='file'])[1]")
 logo = wait.until(EC.presence_of_element_located((By.XPATH, "(//input[@type='file'])[1]")))
 driver.execute_script("arguments[0].scrollIntoView(true);", logo)
 driver.execute_script("arguments[0].style.display = 'block';", logo)
 logo.send_keys("C:/Users/Tulip/OneDrive - TulipTech LTD/Desktop/Test_Logos/logo007.png")
-#crop logo
-set_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[normalize-space()='Set']")))
-set_button.click()
+#crop logo & set
+logo_set_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[normalize-space()='Set']")))
+logo_set_button.click()
+
+
+# favicon upload
+time.sleep(1)
+favicon = wait.until(EC.presence_of_element_located((By.XPATH, "(//input[@type='file'])[1]")))
+driver.execute_script("arguments[0].scrollIntoView(true);", favicon)
+driver.execute_script("arguments[0].style.display = 'block';", favicon)
+favicon.send_keys("C:/Users/Tulip/OneDrive - TulipTech LTD/Desktop/Test_Logos/New_folder/favicon_32.png")
+
 
 # registration email image
-reg_email_image = driver.find_element(By.XPATH, "(//div)[123]")
+time.sleep(1)
+reg_email_image = wait.until(EC.presence_of_element_located((By.XPATH, "(//input[@type='file'])[1]")))
 driver.execute_script("arguments[0].scrollIntoView(true);", reg_email_image)
-driver.execute_script("arguments[0].click();", reg_email_image)
-reg_email_image.send_keys()
+driver.execute_script("arguments[0].style.display = 'block';", reg_email_image)
+reg_email_image.send_keys("C:/Users/Tulip/OneDrive - TulipTech LTD/Desktop/Test_Logos/download.png")
+#crop logo & set
+# time.sleep(1)
+reg_set_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[normalize-space()='Set']")))
+reg_set_button.click()
+
+
+# Registration, Login page logo
+time.sleep(1)
+reg_logo = wait.until(EC.presence_of_element_located((By.XPATH, "(//input[@type='file'])[1]")))
+driver.execute_script("arguments[0].scrollIntoView(true);", reg_logo)
+driver.execute_script("arguments[0].style.display = 'block';", reg_logo)
+reg_logo.send_keys("C:/Users/Tulip/OneDrive - TulipTech LTD/Desktop/Test_Logos/logo007.png")
+#crop logo & set
+# time.sleep(1)
+reg_logo_set_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[normalize-space()='Set']")))
+reg_logo_set_button.click()
+
+
+# ============================================================================================================
 
 # # background color (NN)
 # bg_color = driver.find_element(By.CLASS_NAME, 'choose-font-color-input')
@@ -120,10 +131,28 @@ reg_email_image.send_keys()
 # driver.execute_script("arguments[0].click();", color_hex)
 # driver.execute_script("arguments[0].value = arguments[1];", color_hex, new_value)
 
-# # save button
-# save_btn = driver.find_element(By.CLASS_NAME, "custom-save-btn")
-# driver.execute_script("arguments[0].scrollIntoView(true);", save_btn)
-# save_btn.click()
+# ============================================================================================================
+
+wait.until(EC.visibility_of_element_located((By.XPATH, "//div[@class='home-page']")))
+time.sleep(10)
+
+# ============================================================================================================
+
+# save button
+save_btn = wait.until(EC.visibility_of_element_located((By.XPATH, "//button[normalize-space()='Save & Next']")))
+save_btn_name = save_btn.text
+print(f"save_btn_name: {save_btn_name}")
+driver.execute_script("arguments[0].scrollIntoView(true);", save_btn)
+time.sleep(1)
+driver.execute_script("arguments[0].click();", save_btn) #click save btn
+
+### branding save confirmation
+# time.sleep(10)
+# wait.until(EC.presence_of_element_located((By.ID, "swal2-html-container")))
+
+# home_title = driver.find_element(By.XPATH, "//p[@class='general-info-title']").text
+# print(f"home_title: {home_title}")
+
 
 
 time.sleep(5)
