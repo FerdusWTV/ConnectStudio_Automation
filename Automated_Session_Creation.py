326eb44446e96e5b42b34c473601d10d576d7773
ferdus615 <ferdus615@gmail.com>
2025-09-25 17:25:31 +0600

    organization card added

--- Automated_Session_Creation.py
+++ Automated_Session_Creation.py
@@ -1,3 +1,4 @@
+from http import client
 import time
 import os
 
@@ -52,4 +53,31 @@ assert 'Welcome' in welcome
 
 # click the organizaton button
 org_btn = wait.until(EC.presence_of_element_located((By.XPATH, "(//div[contains(text(),'Organization')])[1]")))
+org_btn_text = org_btn.text
+print(f"org_btn_text: {org_btn_text}")
 driver.execute_script("arguments[0].click();", org_btn)
+
+# ======================================================================================================================
+
+# click the page button
+btn_13 = wait.until(EC.presence_of_element_located((By.XPATH, "//a[normalize-space()='13']")))
+driver.execute_script("arguments[0].scrollIntoView(true);", btn_13)
+driver.execute_script("arguments[0].click();", btn_13)
+
+# ======================================================================================================================
+
+# click the organization card
+
+org_arrow = wait.until(EC.presence_of_element_located((By.XPATH, "(//div[@class='org-card-arrow'])[8]")))
+org_name = driver.find_element(By.XPATH, "//h6[normalize-space()='Automated Test ORG']").text
+print(f"org_name: {org_name}")
+driver.execute_script("arguments[0].click();", org_arrow)
+
+# ======================================================================================================================
+
+# click the client
+client_box = 
+
+
+
+time.sleep(5)
